3438489d2401395d84634f2e8ef95ab4f42c8739
Marina Goto <5m.clover3@gmail.com>
2018-04-15 15:29:23 +0900

    Initial Commit

---
+++ leaders_checkwork_marimon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  leaders_checkwork_marimon
+//
+//  Created by Marina Goto on 2018/04/15.
+//  Copyright © 2018年 lifeistech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
